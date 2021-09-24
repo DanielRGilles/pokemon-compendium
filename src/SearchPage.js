8bc0c8addcb092340ac7ce3632f9f570043aec5c
DanielRGilles <danryangilles@gmail.com>
2021-09-24 13:27:52 -0700

    added dropdown for type searching

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import './App.css';
-import request, { search } from 'superagent';
+import request from 'superagent';
 import PokeList from './components/PokeList';
 import Header from './components/Header';
 import Sort from './components/Sort';
-import Serch from './components/Search';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 export default class SearchPage extends Component {
    state = {
@@ -12,28 +13,38 @@ export default class SearchPage extends Component {
        query: '',
        pokemon: [],
        pokePerpage: '20',
-       isLoading: true
+       isLoading: true,
+       typeQ: 'pokemon'
    }
- componentDidMount = async () => {
-        const response = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?page=1&perPage=20`)
-        this.setState({pokemon: response.body.results, isLoading: false})
+    componentDidMount = async () => {
+         await this.fetchData();
     } 
     handleChange = (e) => {
         this.setState({ query: e.target.value });
+    }
+    handleTypeChange = (e) => {
+        this.setState({ typeQ: e.target.value });
     }
     handleQuantityChange = (e) => {
         this.setState({ pokePerpage: e.target.value});
     }
     handleOrderChange = (e) => {
-        this.setState({ sortOrder: e.target.value });
-        
-    }
-    
+        this.setState({ sortOrder: e.target.value });    
+    } 
+
     handleSubmit = async (e) => {
             e.preventDefault();
-            const search = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.query}&sort=pokemon&direction=${this.state.sortOrder}page=1&perPage=${this.state.pokePerpage}`)
+        await this.fetchData();
+    }
+    handleTypeSubmit = async (e) => {
+            e.preventDefault();
+        await this.fetchData();
+    }  
+    fetchData = async() => {
+            const search = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?${this.state.typeQ}=${this.state.query}&sort=pokemon&direction=${this.state.sortOrder}page=1&perPage=${this.state.pokePerpage}`)
             this.setState({ pokemon: search.body.results, isLoading: false});
-        }
+    }
+  
         
     render() {
         console.log(this.state.pokemon)
@@ -41,7 +52,8 @@ export default class SearchPage extends Component {
             <main className='poke-cnt'>
               <Header/>
             <section className='search-box'>
-              <Serch handleSubmit={this.handleSubmit} handleChange={this.handleChange} query={this.state.query}/>
+              <Search handleSubmit={this.handleSubmit} placeholder={`Search by ${this.state.typeQ}`} handleChange={this.handleChange} />
+              <Dropdown handleChange={this.handleTypeChange} value={this.state.typeQ} options={['pokemon','type_1',]} />
               <Sort handleOrderChange={this.handleOrderChange} sortOrder={this.state.sortOrder} pokePerpage={this.state.pokePerpage} handleQuantityChange={this.handleQuantityChange}/>
             </section>
               <PokeList isLoading={this.state.isLoading} pokemons= {this.state.pokemon} />  
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,10 +5,11 @@ export default class Search extends Component {
         return (
             <>
                  <form onSubmit={this.props.handleSubmit}>
-                <input type='text' value={this.props.query} onChange={this.props.handleChange} />
+                <input type='text' placeholder={this.props.placeholder}  onChange={this.props.handleChange} />
                 <button>Search</button>
             </form>
             </>
         )
     }
 }
+// value={this.props.query}
